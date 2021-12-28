b48717308e2ac80d65140ee5b5b54250f71ea9b5
fish895623 <dan990429@gmail.com>
2021-12-29 00:39:03 +0900

    use pid to disable app duplication

--- src/main/java/com/github/fish895623/commandque/App.java
+++ src/main/java/com/github/fish895623/commandque/App.java
@@ -2,10 +2,14 @@ package com.github.fish895623.commandque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+    // SpringApplication.run(App.class, args);
+    SpringApplication app = new SpringApplication(App.class);
+    app.addListeners(new ApplicationPidFileWriter());
+    app.run(args);
   }
 }
