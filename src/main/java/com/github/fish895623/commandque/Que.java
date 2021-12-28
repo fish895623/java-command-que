cf62fd41fc07690e78b411ad235004b46df31e43
fish895623 <dan990429@gmail.com>
2021-12-29 00:22:01 +0900

    Rename

--- src/main/java/com/github/fish895623/commandque/Que.java
+++ src/main/java/com/github/fish895623/commandque/Que.java
@@ -1,5 +1,7 @@
 package com.github.fish895623.commandque;
 
+import com.github.fish895623.commandque.data.InfoVO;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Que {
-  // 123
-  @RequestMapping(method = RequestMethod.POST, path = "/a")
-  public InfoVO aa(@RequestBody InfoVO body) {
+  @RequestMapping(method = RequestMethod.POST, path = "/PORT")
+  public InfoVO post(@RequestBody InfoVO body) {
     body.setT(body.getName() + "123");
     return body;
   }
