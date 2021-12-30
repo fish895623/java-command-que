abc2bb8955d0753ddc76c0ec88645c9d556a1914
fish895623 <dan990429@gmail.com>
2021-12-30 10:38:33 +0900

    Rearrange

--- src/main/java/com/github/fish895623/commandque/Que.java
+++ src/main/java/com/github/fish895623/commandque/Que.java
@@ -1,8 +1,7 @@
 package com.github.fish895623.commandque;
 
-import com.github.fish895623.commandque.data.InfoVO;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
