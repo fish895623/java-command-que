d2f763e5970419145c28795386b6ef0b86c7b649
fish895623 <dan990429@gmail.com>
2022-01-28 17:07:57 +0900

    import gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,32 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
-    id("org.springframework.boot") version "2.6.1" apply true
-    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply true
-    id("java")
+  id("org.springframework.boot") version "2.6.1"
+  id("io.spring.dependency-management") version "1.0.11.RELEASE" apply true
+  id("java")
 }
 
 group = "com.github.fish895623"
 version = "0.0.102"
 
-tasks.bootJar {
-  archiveBaseName.set("command")
-  archiveVersion.set("")
+tasks {
+  withType<BootJar> {
+    archiveBaseName.set("command")
+    archiveVersion.set("")
+  }
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:2.6.2")
-    implementation("org.xerial:sqlite-jdbc:3.36.0.2")
-    developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.2")
+  implementation("org.springframework.boot:spring-boot-starter-web:2.6.3")
+  implementation("org.xerial:sqlite-jdbc:3.36.0.2")
+  developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.2")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.3")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
