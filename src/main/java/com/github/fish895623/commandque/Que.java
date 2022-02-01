64bfa8e08dea5b333fd1b6ae2a7389d017e5740d
fish895623 <dan990429@gmail.com>
2022-02-01 21:03:26 +0900

    Separate parts

--- src/main/java/com/github/fish895623/commandque/Que.java
+++ src/main/java/com/github/fish895623/commandque/Que.java
@@ -3,24 +3,55 @@ package com.github.fish895623.commandque;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class Que {
   private final static Logger logger = LogManager.getLogger(Que.class);
+  private static Connection connection = null;
+  private static Statement statement;
 
-  private final Connection connection = null;
-  private final String dbFileName;
-
-  public Que(String dbFileName) {
-    this.dbFileName = dbFileName;
-  }
+  private String dbFileName;
 
   public static String te() {
     return "asdf";
   }
 
   public static void main(String[] args) {
-    logger.warn("asdf");
-    logger.info("aaa");
+    Que a = new Que();
+    a.setDBFileName("asdf");
+    a.start();
+  }
+
+  public ResultSet getData(String query) {
+    ResultSet rs = null;
+
+    try {
+      rs = statement.executeQuery(query);
+
+      logger.info("Executing Query");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      logger.error("ERROR on getting Data");
+    }
+
+    return rs;
+  }
+
+  public void start() {
+    try {
+      Class.forName("org.sqlite.JDBC");
+      connection = DriverManager.getConnection("jdbc:sqlite:" + dbFileName);
+      statement = connection.createStatement();
+
+      logger.info("Connect to sqlite");
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+
+      logger.error("ClassNotFound, SQLException");
+    }
+  }
+
+  public void setDBFileName(String val) {
+    this.dbFileName = val;
   }
 }
