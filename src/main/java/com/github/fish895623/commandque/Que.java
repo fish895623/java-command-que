4729cff2a060c213eb983dcc13646e0e391ae65b
fish895623 <dan990429@gmail.com>
2022-02-01 12:48:59 +0900

    Add sql, log4j setup

--- src/main/java/com/github/fish895623/commandque/Que.java
+++ src/main/java/com/github/fish895623/commandque/Que.java
@@ -1,6 +1,20 @@
 package com.github.fish895623.commandque;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+
 public class Que {
+  private final static Logger logger = LogManager.getLogger(Que.class);
+
+  private final Connection connection = null;
+  private final String dbFileName;
+
+  public Que(String dbFileName) {
+    this.dbFileName = dbFileName;
+  }
+
   public static String te() {
     return "asdf";
   }
