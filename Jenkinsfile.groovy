5a4165a7d973678ab8cf3d48d62be2c33d8f5f76
fish895623 <34264960+fish895623@users.noreply.github.com>
2022-02-03 15:41:45 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,10 @@
 pipeline {
-    agent none
+    agent all
     stages {
-        stage('Example Build') {
-            agent { docker 'maven:3.8.1-adoptopenjdk-11' }
-            steps {
-                echo 'Hello, Maven'
-                sh 'mvn --version'
-            }
-        }
         stage('Example Test') {
-            agent { docker 'openjdk:11-jdk' }
             steps {
                 echo 'Hello, JDK'
-                sh 'java -version'
+                sh './gradlew bootJar'
             }
         }
     }
