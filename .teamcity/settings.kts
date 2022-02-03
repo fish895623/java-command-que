f4f107b339c6e4e6547b4297baeb6819d837fd93
fish895623 <dan990429@gmail.com>
2022-02-03 23:59:19 +0900

    build2

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
@@ -34,6 +35,43 @@ object Build : BuildType({
     }
   }
 
+  features {
+    commitStatusPublisher {
+      vcsRootExtId = "${DslContext.settingsRoot.id}"
+      publisher = github {
+        githubUrl = "https://api.github.com"
+        authType = personalToken {
+          token =
+            "credentialsJSON:fb871c07-1d54-4a49-8fb8-26a235b2b988"
+        }
+      }
+    }
+    pullRequests {
+      vcsRootExtId = "${DslContext.settingsRoot.id}"
+      provider = github {
+        authType = token {
+          token =
+            "credentialsJSON:fb871c07-1d54-4a49-8fb8-26a235b2b988"
+        }
+        filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+      }
+    }
+  }
+})
+
+object Build_2 : BuildType({
+  name = "Build 2"
+  steps {
+    script {
+      scriptContent = """
+        echo abcd
+      """.trimIndent()
+    }
+  }
+  triggers {
+    vcs {
+    }
+  }
   features {
     commitStatusPublisher {
       vcsRootExtId = "${DslContext.settingsRoot.id}"
