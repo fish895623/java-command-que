29c3b018a3cc707222d65f1f1a1631012f99189d
teamcity <teamcity@TeamCity>
2022-02-03 22:55:28 +0900

    TeamCity change in 'Java Command Que' project: build features of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
@@ -58,6 +60,15 @@ object Build : BuildType({
                 authType = personalToken {
                     token = "credentialsJSON:fb871c07-1d54-4a49-8fb8-26a235b2b988"
                 }
+            }
+        }
+        pullRequests {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:fb871c07-1d54-4a49-8fb8-26a235b2b988"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
             }
         }
     }
